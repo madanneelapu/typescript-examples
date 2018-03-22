02f51e7fca80c225ae03e5610d63a75b2be2212b
Madan Neelapu <madanneelapu@gmail.com>
2018-03-22 08:35:24 +0530

    introducing types

---
+++ 003-type-checking.js
@@ -0,0 +1,15 @@
+/*var a;
+
+a = 10;
+a = true;
+a = {};
+
+*/
+var a;
+a = 10;
+//a = true; //Will result in error
+var b;
+b = true;
+var c;
+c = "Madan";
+c = "M";
---
+++ 003-type-checking.ts
@@ -0,0 +1,19 @@
+/*var a;
+
+a = 10;
+a = true;
+a = {};
+
+*/
+
+var a: number;
+
+a = 10;
+//a = true; //Will result in error
+
+var b: boolean;
+b=true;
+
+var c: string;
+c="Madan";
+c="M";
