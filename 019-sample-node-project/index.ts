8e7d967f6435d952051ad9831d916a82437bdb7f
Madan Neelapu <madanneelapu@gmail.com>
2018-03-30 06:57:06 +0530

    NodeJS project and TypeScript

---
+++ 019-sample-node-project/index.ts
@@ -0,0 +1,4 @@
+import {Person3} from './person';
+
+var person = new Person3('Madan','Neelapu');
+console.log(person.firstname);
---
+++ 019-sample-node-project/person.ts
@@ -0,0 +1,12 @@
+
+
+export class Person3{
+    firstname: string;
+    lastname: string;
+
+    constructor(firstname:string, lastname:string){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+}
