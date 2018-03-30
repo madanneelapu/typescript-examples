d00960df55a73f34dd5eca19e6f7370c9b9e996e
Madan Neelapu <madanneelapu@gmail.com>
2018-03-30 07:26:57 +0530

    adding dependencies and type definitions

--- 019-sample-node-project/index.ts
+++ 019-sample-node-project/index.ts
@@ -1,4 +1,10 @@
 import {Person3} from './person';
+import * as _ from 'lodash'; 
 
 var person = new Person3('Madan','Neelapu');
 console.log(person.firstname);
+
+//Using lodash
+
+var myArray = [1,2,3,4,5];
+console.log(_.reverse(myArray));
