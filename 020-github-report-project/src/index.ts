2a29691cbd011c95ed4a8e3df46e6f95c02747f1
Madan Neelapu <madanneelapu@gmail.com>
2018-03-31 08:56:17 +0530

    using CLI args

--- 020-github-report-project/src/index.ts
+++ 020-github-report-project/src/index.ts
@@ -1,15 +1,24 @@
-import {GithubApiService} from './GithubApiService';
+import { GithubApiService } from './GithubApiService';
 import * as _ from "lodash";
 import { User } from './User';
 import { Repo } from './Repo';
 
 let svc = new GithubApiService();
-svc.getUserInfo("madanneelapu",(user: User)=>{    
-    svc.getRepos("madanneelapu",(repos: Repo[]) => {
-        let sortedRepos = _.sortBy(repos, [(repo:Repo) => repo.size * -1]);
-        let filteredRepos = _.take(sortedRepos, 3);
-        user.repos = filteredRepos;
-        console.log(user);
-    });
-});
+let cliArgs = process.argv; //Reading command line arguments. Returns an array.
 
+if (cliArgs.length < 3) { //custom arguments would start from position 2.
+    console.log("Please pass the username as an argument.");
+} else {
+
+    let username = cliArgs[2];
+
+    svc.getUserInfo(username, (user: User) => {
+        svc.getRepos(username, (repos: Repo[]) => {
+            let sortedRepos = _.sortBy(repos, [(repo: Repo) => repo.size * -1]);
+            let filteredRepos = _.take(sortedRepos, 3);
+            user.repos = filteredRepos;
+            console.log(user);
+        });
+    });
+
+}
