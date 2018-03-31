197fe57d3ed5bf5d2ce77bec8de8d2f9a3aef020
Madan Neelapu <madanneelapu@gmail.com>
2018-03-31 08:46:59 +0530

    sort ad filter repos

--- 020-github-report-project/src/index.ts
+++ 020-github-report-project/src/index.ts
@@ -1,11 +1,14 @@
 import {GithubApiService} from './GithubApiService';
+import * as _ from "lodash";
 import { User } from './User';
 import { Repo } from './Repo';
 
 let svc = new GithubApiService();
 svc.getUserInfo("madanneelapu",(user: User)=>{    
     svc.getRepos("madanneelapu",(repos: Repo[]) => {
-        user.repos = repos;
+        let sortedRepos = _.sortBy(repos, [(repo:Repo) => repo.size * -1]);
+        let filteredRepos = _.take(sortedRepos, 3);
+        user.repos = filteredRepos;
         console.log(user);
     });
 });
